5b819325f819bc29b93bfe797256bf7d79b56218
Evgeny Rov <bitsinmyhead@gmail.com>
2020-12-22 14:31:32 +0800

    added post search feature

--- src/routes/Home/Posts.tsx
+++ src/routes/Home/Posts.tsx
@@ -1,10 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppState, postsActions } from '../../redux';
+import { AppState, Post, postsActions } from '../../redux';
+import { filterPostsByString } from '../../utils';
+
+const PostItem: React.FC<Post> = ({ id, userId, title, body }) => {
+  return (
+    <li
+      className="grid gap-4 w-full max-w-lg m-2 items-start p-4 rounded-lg bg-white shadow-md"
+      key={id}
+    >
+      <div className="flex justify-between border-b-2">
+        <span className="italic">user id:</span>
+        <span>{userId}</span>
+      </div>
+      <div className="text-center">
+        <p>{title}</p>
+      </div>
+      <div className="text-center truncate">
+        <span>{body}</span>
+      </div>
+    </li>
+  );
+};
 
 const Posts: React.FC = () => {
-  const { posts } = useSelector((state: AppState) => state.posts);
+  const [searchQuery, setSearchQuery] = useState('');
   const dispatch = useDispatch();
+  const posts = useSelector((state: AppState) =>
+    filterPostsByString(state.posts.data, searchQuery)
+  );
 
   useEffect(() => {
     dispatch(postsActions.startFetching());
@@ -13,26 +37,18 @@ const Posts: React.FC = () => {
   return (
     <div className="grid w-full place-items-center p-4">
       <h1>POSTS PAGE</h1>
-      <ul className="w-full mt-4 flex flex-wrap justify-evenly">
-        {posts.map((post) => {
-          return (
-            <li
-              className="grid gap-4 w-full max-w-lg m-2 items-start p-4 rounded-lg bg-white shadow-md"
-              key={post.id}
-            >
-              <div className="flex justify-between border-b-2">
-                <span className="italic">user id:</span>
-                <span>{post.userId}</span>
-              </div>
-              <div className="text-center">
-                <p>{post.title}</p>
-              </div>
-              <div className="text-center truncate">
-                <span>{post.body}</span>
-              </div>
-            </li>
-          );
-        })}
+      <div className="w-full mt-2 px-2 max-w-lg">
+        <input
+          className="bg-white w-full rounded-full px-4 py-1 shadow-md"
+          type="text"
+          placeholder="Search post..."
+          name="search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+      <ul className="w-full mt-6 flex flex-wrap justify-evenly">
+        {posts.map(PostItem)}
       </ul>
     </div>
   );
